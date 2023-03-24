f19a4291d725d3bdaf551d17b7cde0ae8521e490
unknown <tranduyphat080300@gmail.com>
2023-03-24 22:04:33 +0200

    added cors and moved .gitignore to root directory

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 
 const PORT = 4000;
 const app = express();
+
+app.use(cors);
 
 await mongoose
   .connect(
