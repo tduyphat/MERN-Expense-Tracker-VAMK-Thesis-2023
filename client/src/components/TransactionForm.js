a8ef4d58b44b421931a247c81d31ad4d396552e5
unknown <tranduyphat080300@gmail.com>
2023-04-02 23:59:25 +0300

    design input form

--- client/src/components/TransactionForm.js
+++ client/src/components/TransactionForm.js
@@ -1,29 +1,48 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { TextField } from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers";
+import Button from "@mui/material/Button";
 
 export default function TransactionForm() {
+  function handleChange() {}
   return (
     <Card sx={{ minWidth: 275, marginTop: 10 }}>
       <CardContent>
-        {/* <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography> */}
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <Typography variant="h6">Add New Transaction</Typography>
+        <form>
+          <TextField
+            sx={{ marginRight: 5 }}
+            id="outlined-basic"
+            label="Amount"
+            size="small"
+            variant="outlined"
+          />
+          <TextField
+            sx={{ marginRight: 5 }}
+            id="outlined-basic"
+            label="Description"
+            size="small"
+            variant="outlined"
+          />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Transaction Date"
+              inputFormat="MM/DD/YYYY"
+              onChange={handleChange}
+              renderInput={(params) => <TextField {...params} />}
+              size="small"
+              sx={{ marginRight: 5 }}
+            />
+          </LocalizationProvider>
+          <Button type="submit" variant="contained">
+            Submit
+          </Button>
+        </form>
       </CardContent>
     </Card>
   );
